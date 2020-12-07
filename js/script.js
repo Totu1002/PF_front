70095c9f89def506764b867889947ae0947fe677
Totu1002_Mac <totu.enjoy.music@gmail.com>
2020-12-08 01:29:28 +0900

    [Add]js/topボタン追加

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//トップに戻るボタンアクション
+$(function() {
+    $('#back a').on('click',function(event){
+      $('body, html').animate({
+        scrollTop:0
+      }, 800);
+      event.preventDefault();
+    });
+});
