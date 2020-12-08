78d60df6c6b48deca0f642af39839747dc7c8255
Totu1002_Mac <totu.enjoy.music@gmail.com>
2020-12-08 23:36:23 +0900

    [Add]js/ハンバーガーメニュー実装

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ $(function() {
       event.preventDefault();
     });
 });
+
+//ハンバーガーメニュー
+$(function(){
+    $("#toggle").click(function(){
+      $("#menu").slideToggle();
+      return false;
+    });
+    $(window).resize(function(){
+      var win = $(window).width();
+      var p = 480;
+      if(win > p){
+        $("#menu").show();
+      }
+    });
+});
